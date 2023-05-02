8eb48e12d9afad5922687915b6738f4131cfc36d
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-05-02 12:32:32 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,312 @@
+import streamlit as st
+import json
+import toml
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+from ast import Index
+import json
+import re
+import requests
+
+def swap_xy(xy):
+  new_xy = re.findall("\[(.*?)\]", xy)
+  if new_xy:
+    match = new_xy[0]
+    match = match.split(sep = ",")
+    match = match[::-1]
+    new_xy = ('[' + match[0].strip() + ',' + match[1] + ']')
+    return (new_xy)
+  else:
+    new_xy=""
+    return (new_xy)
+
+def parse_wardley_map(map_text):
+    lines = map_text.strip().split("\n")
+    title, evolution, anchors, components, nodes, links, evolve, pipelines, pioneers, market, blueline, notes, annotations, comments, style = [], [], [], [], [], [], [], [], [], [], [], [], [], [], []
+    current_section = None
+
+    for line in lines:
+        if line.startswith("//"):
+            comments.append(line)
+
+        elif line.startswith("evolution"):
+            evolution.append(line)
+
+        elif "+<>" in line:
+            blueline.append(line)
+
+        elif line.startswith("title"):
+            name = ' '.join(line.split()[1:])
+            title.append(name)
+
+        elif line.startswith("anchor"):
+            name = line[line.find(' ') + 1:line.find('[')].strip()
+            anchors.append(name)
+
+        elif line.startswith("component"):
+            stage = ""
+            pos_index = line.find("[")
+            if pos_index != -1:
+                new_c_xy = swap_xy(line)
+                number = json.loads(new_c_xy)
+                if 0 <= number[0] <= 0.17:
+                    stage = "genesis"
+                elif 0.18 <= number[0] <= 0.39:
+                    stage = "custom"
+                elif 0.31 <= number[0] <= 0.69:
+                    stage = "product"
+                elif 0.70 <= number[0] <= 1.0:
+                    stage = "commodity"
+                else:
+                    visibility = ""
+                if 0 <= number[1] <= 0.20:
+                    visibility = "low"
+                elif 0.21 <= number[1] <= 0.70:
+                    visibility = "medium"
+                elif 0.71 <= number[1] <= 1.0:
+                    visibility = "high"
+                else:
+                    visibility = ""               
+            else:
+                new_c_xy = ""
+
+            name = line[line.find(' ') + 1:line.find('[')].strip()
+
+            label_index = line.find("label")
+            if label_index != -1:
+                label = line[label_index+len("label")+1:]
+                label = swap_xy(label)
+            else:
+                label = ""
+
+            components.append({"name": name, "description": "", "evolution": stage, "visibility": visibility, "positionxy": new_c_xy, "labelxy": label})
+
+        elif line.startswith("pipeline"):
+            new_c_xy = swap_xy(line)
+            name = line[line.find(' ') + 1:line.find('[')].strip()
+            pipelines.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": ""})
+
+        elif line.startswith("links"):
+            links.append(line)
+
+        elif line.startswith("evolve"):
+            new_c_xy = swap_xy(line)
+            name = re.findall(r'\b\w+\b\s(.+?)\s\d', line)[0]
+            label_index = line.find("label")
+            if label_index != -1:
+                label = line[label_index+len("label")+1:]
+            else:
+                label = ""
+            label = swap_xy(label)
+            evolve.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": label})
+
+        elif line.startswith("pioneer"):          
+            pioneers.append(line)
+
+        elif line.startswith("note"):
+            name = line[line.find(' ') + 1:line.find('[')].strip()
+            pos_index = line.find("[")
+            if pos_index != -1:
+                new_c_xy = swap_xy(line)
+            else:
+                new_c_xy = ""
+            notes.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": ""})   
+                  
+        elif line.startswith("annotations"):
+            new_c_xy = swap_xy(line)
+            annotations.append({"name": line, "description": "", "positionxy": new_c_xy})
+
+        elif line.startswith("annotation"):
+            new_c_xy = swap_xy(line)
+            number = re.findall(r'\d+', line)
+            name = line[line.index(']')+1:].lstrip()
+            annotations.append({"number": number[0], "name": name, "description": "", "positionxy": new_c_xy})
+
+        elif line.startswith("market"):
+            name = line[line.find(' ') + 1:line.find('[')].strip()
+            new_c_xy = swap_xy(line)
+            label_index = line.find("label")
+            if label_index != -1:
+                label = line[label_index+len("label")+1:]
+            else:
+                label = ""
+            label = swap_xy(label)
+            market.append({"name": name, "description": "", "positionxy": new_c_xy, "labelxy": label})
+
+        elif line.startswith("style"):
+            style.append(line)
+
+        elif "->" in line:
+            source, target = line.strip().split("->")
+            source = source.strip()
+            target = target.strip()
+            links.append({"source": source, "target": target})
+        else:
+            continue
+
+    return {
+        "title" : title,
+        "anchors" : anchors,
+        "evolution" : evolution,
+        "components": components,
+        "links": links,
+        "evolve": evolve,
+        "markets": market,
+        "pipelines": pipelines,
+        "pioneers": pioneers,
+        "notes": notes,
+        "blueline": blueline,
+        "style": style,
+        "annotations": annotations,
+        "comments": comments,
+    }
+
+st.set_page_config(
+    page_title="JSON to TOML file converter"
+)
+
+with st.sidebar:
+    selected = option_menu(
+        "Choose conversion",
+        ["WM to JSON", "WM to TOML", "JSON to TOML"],
+        icons=["gear"],
+        # menu_icon="bookmark-fill",
+        menu_icon="robot",
+        default_index=0,
+    )
+
+if selected == "JSON to TOML":
+    st.title("JSON to TOML file converter")
+    st.write(
+        """  
+
+            """
+    )
+
+    st.write(
+        """  
+    Let's convert your Wardley Map in JSON to TOML
+
+            """
+    )
+
+    st.write(
+        """  
+
+            """
+    )
+    json_file = st.file_uploader("UPLOAD JSON FILE")
+    st.info(
+        f"""
+                👆 Upload your json file.
+                
+                """
+    )
+
+
+    if json_file is not None:
+        json_text = json_file.read()
+
+        st.write("JSON CONTENT")
+        st.code(json.loads(json_text))
+
+        toml_content = toml.dumps(json.loads(json_text))
+        st.write("TOML FILE CONTENT")
+        st.code(toml_content, language="toml")
+        toml_file_name = json_file.name.replace(".json", ".toml")
+        st.download_button(
+            "DOWNLOAD TOML FILE", data=toml_content, file_name=toml_file_name
+        )
+        
+elif selected == "WM to TOML":
+    st.title("WM to TOML Converter")
+    st.write(
+        """  
+            """
+    )
+    st.write(
+        """  
+    Let's convert your Wardley Map in WM to TOML
+            """
+    )
+    
+    st.write(
+        """  
+            """
+    )
+    
+    # Map ID from onlinewardleymapping
+    map_id=''
+    map_id = st.text_input("Enter the ID of the Wardley Map: For example https://onlinewardleymaps.com/#clone:OXeRWhqHSLDXfOnrfI, enter: OXeRWhqHSLDXfOnrfI", value="OXeRWhqHSLDXfOnrfI")
+    
+    # Fetch map using onlinewardleymapping api
+    url = f"https://api.onlinewardleymaps.com/v1/maps/fetch?id={map_id}"
+    response = requests.get(url)
+    
+    # Check if the map was found
+    if response.status_code == 200:
+        map_data = response.json()
+        wardley_map_text = map_data['text']
+
+# Parse the Wardley map text
+        parsed_map = parse_wardley_map(wardley_map_text)
+        wardley_map_toml = toml.dumps(parsed_map)
+        st.write("TOML FILE CONTENT")
+        st.code(wardley_map_toml, language="toml")  
+        
+        toml_file_name = map_id + '.toml'
+        st.download_button(
+            "DOWNLOAD TOML FILE",
+            data=wardley_map_toml,
+            file_name=toml_file_name
+        )  
+
+elif selected == "WM to JSON":
+    st.title("WM to JSON File Converter")
+    st.write(
+        """  
+            """
+    )
+    st.write(
+        """  
+    Let's convert your Wardley Map in WM to JSON
+            """
+    )
+    
+    st.write(
+        """  
+            """
+    )
+    
+    # Map ID from onlinewardleymapping
+    map_id=''
+    map_id = st.text_input("Enter the ID of the Wardley Map: For example https://onlinewardleymaps.com/#clone:OXeRWhqHSLDXfOnrfI, enter: OXeRWhqHSLDXfOnrfI", value="OXeRWhqHSLDXfOnrfI")
+    
+    # Fetch map using onlinewardleymapping api
+    url = f"https://api.onlinewardleymaps.com/v1/maps/fetch?id={map_id}"
+    response = requests.get(url)
+    
+    # Check if the map was found
+    if response.status_code == 200:
+        map_data = response.json()
+        wardley_map_text = map_data['text']
+
+# Parse the Wardley map text
+        parsed_map = parse_wardley_map(wardley_map_text)
+
+
+# Print the JSON
+        print(parsed_map)
+        
+        # Convert the parsed map to JSON
+        wardley_map_json = json.dumps(parsed_map, indent=2)
+        st.write("JSON FILE CONTENT")
+        st.json(wardley_map_json, expanded=False)  
+        
+        json_file_name = map_id + '.json'
+        st.download_button(
+            "DOWNLOAD JSON FILE",
+            data=wardley_map_json,
+            file_name=json_file_name
+        )
