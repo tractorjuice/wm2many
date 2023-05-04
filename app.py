95fb41bb196b545d36d6ad0fec1451b791b1cb79
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-05-04 19:16:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import streamlit as st
+from streamlit_option_menu import option_menu
 import json
 import toml
-import streamlit as st
-from streamlit_option_menu import option_menu
 
 from ast import Index
 import json
