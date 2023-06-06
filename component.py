6e92eaf6bbf3035b8f1fb38b616afe4bafe48a64
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-06 08:41:03 +0100

    Create component.py

---
+++ component.py
@@ -0,0 +1,38 @@
+class Component:
+	def __init__(self, name):
+		self.name = name
+		self.component = []
+		self.links = []
+		self.inertia = []
+		self.visibility = []
+		self.evolution = []
+        
+	def get(self, self.name)
+		return(self.name = name,
+		self.component = [],
+		self.links = [],
+		self.inertia = [],
+		self.visibility = [],
+		self.evolution = [])
+		
+	def add_link(self, link):
+		self.links.append(link)
+	
+	def delete_link(self, link):
+		self.links.append(link)
+        
+	def add(self, name):
+		return
+	
+	def delete(self, name):
+		return
+		
+	def visibility(self, visibility):
+		return
+		
+	def evolution(self, evolution):
+		return
+		
+    def inertia(self, name)
+		self.inertia.append(name)
+		return
