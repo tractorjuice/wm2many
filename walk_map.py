a26ac2ac05b24b2950cafb335ffdec5b60695a58
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-06 07:02:16 +0100

    Update walk_map.py

--- walk_map.py
+++ walk_map.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
-def dfs(map, element, start, visited=None):
+def dfs(graph, start, visited=None):
     if visited is None:
         visited = set()
 
     visited.add(start)
 
-    for link in map[element]:
-        if element['src'] == start and element['tgt'] not in visited:
-            dfs(map, link['tgt'], visited)
+    for link in graph['links']:
+        if link['src'] == start and link['tgt'] not in visited:
+            dfs(graph, link['tgt'], visited)
 
     return visited
