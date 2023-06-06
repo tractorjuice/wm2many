40fc199e3c9f111c0b9e24d183625532539fd333
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2023-06-06 07:23:59 +0100

    Update wardley.py

--- wardley.py
+++ wardley.py
@@ -17,7 +17,14 @@ class WardleyMap:
         :param owm: A string representing the original Wardley Map
         :type owm: str
         """
-        # Insert the parsing logic here...
+        
+        # Assuming owm is a dictionary object with all the required keys
+        self.title = owm.get('title', '')
+        self.edges = owm.get('edges', [])
+        self.nodes = owm.get('nodes', {})
+        self.warnings = []
+        # insert additional attributes as per your original code here...
+        
 
     def plot(self):
         """
@@ -28,7 +35,102 @@ class WardleyMap:
         :return: A matplotlib figure and axes representing the plot
         :rtype: (matplotlib.figure.Figure, matplotlib.axes._subplots.AxesSubplot)
         """
-        fig, ax = plt.subplots(figsize=(24,15))
-        # Insert the logic for generating the plot here...
+        
+        if wm.title:
+            plt.title(wm.title)
+        plt.xlim(0, 1)
+        plt.ylim(0, 1.1)
+        # Plot the lines
+        l = []
+        for edge in wm.edges:
+            if edge[0] in wm.nodes and edge[1] in wm.nodes:
+                n_from = wm.nodes[edge[0]]
+                n_to = wm.nodes[edge[1]]
+                l.append([ (n_from['mat'],n_from['vis']), (n_to['mat'],n_to['vis']) ])
+            else:
+                for n in edge:
+                    if n not in wm.nodes:
+                        wm.warnings.append(f"Could not find component called {n}!")
+        if len(l) > 0:
+            lc = LineCollection(l, color=matplotlib.rcParams['axes.edgecolor'], lw=0.5)
+            ax.add_collection(lc)
+
+        # Plot blue lines
+        b = []
+        for blueline in wm.bluelines:
+            if blueline[0] in wm.nodes and blueline[1] in wm.nodes:
+                n_from = wm.nodes[blueline[0]]
+                n_to = wm.nodes[blueline[1]]
+                b.append([ (n_from['mat'],n_from['vis']), (n_to['mat'],n_to['vis']) ])
+            else:
+                for n in blueline:
+                    if n not in wm.nodes:
+                        wm.warnings.append(f"Could not find blueline component called {n}!")
+        if len(b) > 0:
+            lc = LineCollection(b, color='blue', lw=1)
+            ax.add_collection(lc)
+
+        # Plot Evolve
+        e = []
+        for evolve_title, evolve in wm.evolves.items():
+            if evolve_title in wm.nodes:
+                n_from = wm.nodes[evolve_title]
+                e.append([ (n_from['mat'],n_from['vis']), (evolve['mat'], n_from['vis']) ])
+            else:
+                wm.warnings.append(f"Could not find evolve component called {evolve_title}!")
+        if len(e) > 0:
+            lc = LineCollection(e, color='red', lw=.5, linestyles='dotted')
+            ax.add_collection(lc)
+        # Add the nodes:
+        for node_title in wm.nodes:
+            n = wm.nodes[node_title]
+            if n['type'] == 'component':
+                plt.plot(n['mat'], n['vis'], marker='o', color=matplotlib.rcParams['axes.facecolor'], 
+                    markeredgecolor=matplotlib.rcParams['axes.edgecolor'], markersize=8, lw=1)
+                ax.annotate(node_title, fontsize=matplotlib.rcParams['font.size'], fontfamily=matplotlib.rcParams['font.family'],
+                    xy=(n['mat'], n['vis']), xycoords='data',
+                    xytext=(n['label_x'], n['label_y']), textcoords='offset pixels',
+                    horizontalalignment='left', verticalalignment='bottom')
+
+        # Add the evolve nodes:
+        for evolve_title, evolve in wm.evolves.items():
+            if evolve_title in wm.nodes:
+                n = wm.nodes[evolve_title]
+                plt.plot(evolve['mat'], n['vis'], marker='o', color=matplotlib.rcParams['axes.facecolor'], markeredgecolor='red', markersize=8, lw=1)
+                ax.annotate(evolve_title, fontsize=matplotlib.rcParams['font.size'], fontfamily=matplotlib.rcParams['font.family'],
+                    xy=(evolve['mat'], n['vis']), xycoords='data',
+                    xytext=(n['label_x'], n['label_y']), textcoords='offset pixels',
+                    horizontalalignment='left', verticalalignment='bottom')
+            else:
+                wm.warnings.append(f"Node '{evolve_title}' does not exist in the map.")
+
+        # Add the pipeline nodes:
+        for pipeline_title, pipeline in wm.pipelines.items():
+            if pipeline_title in wm.nodes:
+                n = wm.nodes[pipeline_title]
+                plt.plot(n['mat'], n['vis'], marker='s', color=matplotlib.rcParams['axes.facecolor'], markersize=8, lw=.5)  
+            else:
+                wm.warnings.append(f"Node '{pipeline_title}' does not exist in the map.")
+
+        # Plot Pipelines
+        for pipeline_title, pipeline in wm.pipelines.items():
+            if pipeline_title in wm.nodes:
+                n_from = wm.nodes[pipeline_title]
+                rectangle = patches.Rectangle((pipeline['start_mat'], n_from['vis']-0.02), pipeline['end_mat']-pipeline['start_mat'], 0.02, fill=False, lw=0.5)
+                ax.add_patch(rectangle)
+            else:
+                wm.warnings.append(f"Could not find pipeline component called {pipeline_title}!")
+
+        # Add the notes:
+        for note in wm.notes:
+            plt.text(note['mat'], note['vis'], note['text'], fontsize=matplotlib.rcParams['font.size'], fontfamily=matplotlib.rcParams['font.family'])
+        plt.yticks([0.0,0.925], ['Invisible', 'Visible'], rotation=90, verticalalignment='bottom')
+        plt.ylabel('Visibility', fontweight='bold')
+        plt.xticks([0.0, 0.17,0.4, 0.70], ['Genesis', 'Custom-Built', 'Product\n(+rental)', 'Commodity\n(+utility)'], ha='left')
+        plt.xlabel('Evolution', fontweight='bold')
+        plt.tick_params(axis='x', direction='in', top=True, bottom=True, grid_linewidth=1)
+        plt.grid(visible=True, axis='x', linestyle='--')
+        plt.tick_params(axis='y', length=0)
+
         return fig, ax
 
