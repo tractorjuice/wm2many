7bc9ed778923c54686a03f78185fc8bdb1aaa833
tractorjuice <129532814+tractorjuice@users.noreply.github.com>
2024-06-17 19:21:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -700,6 +700,6 @@ elif selected == "Animate Map":
     parsed_map = parse_wardley_map(st.session_state.map_text)
     ani = animate_wardley_map(parsed_map)
 
-    # Display animation in Streamlit (using iframe)
-    iframe_html = f'<iframe srcdoc="{ani.to_jshtml()}" width="100%" height="600px"></iframe>'
-    st.markdown(iframe_html, unsafe_allow_html=True)
+    # Display animation in Streamlit
+    ani_html = ani.to_jshtml()
+    components.html(ani_html, height=600)
